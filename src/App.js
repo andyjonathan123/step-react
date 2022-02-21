e7e6edc8e9241a0181aff43d83dd9a0729fe4220
andyjonathan123 <jonathanpakpahan1234@gmail.com>
2022-02-21 15:26:54 +0700

    ingin dihapus

--- src/App.js
+++ src/App.js
@@ -31,6 +31,10 @@ class App extends Component {
       <div>
         <p>Perkenalkan Nama saya adalah {this.state.user.name} </p>
         <p>Perkenalkan Nama saya adalah {this.state.user.username} </p>
+        <p>Perkenalkan Nama saya adalah {this.state.user.name} </p>
+        <p>Perkenalkan Nama saya adalah {this.state.user.username} </p>
+        <p>Perkenalkan Nama saya adalah {this.state.user.name} </p>
+        <p>Perkenalkan Nama saya adalah {this.state.user.username} </p>
         
       </div>
     );
